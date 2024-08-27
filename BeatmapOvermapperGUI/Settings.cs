cafff162f41696be58774c37498629983b70f15d
yaggod <yaggod@yandex.ru>
2024-08-27 14:20:17 +0300

    small changes in settings class

--- BeatmapOvermapperGUI/Settings.cs
+++ BeatmapOvermapperGUI/Settings.cs
@@ -1,7 +1,27 @@
-﻿namespace BeatmapOvermapperGUI
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace BeatmapOvermapperGUI
 {
 	static class Settings
 	{
-		public static string SongsFolder { get; set; } = "F:\\osu\\songs";
+		static Settings()
+		{
+			try
+			{
+				var osuProcess = Process.GetProcessesByName("osu!")[0];
+				var osuExecutable = osuProcess.MainModule.FileName;
+				OsuDirectory = Path.GetDirectoryName(osuExecutable);
+			}
+			catch
+			{
+				MessageBox.Show("Osu is not running", "Cant find osu process", MessageBoxButton.OK, MessageBoxImage.Error);
+				throw;
+			}
+		}
+
+		public static string OsuDirectory { get; private set; }
+		public static string SongsFolder => Path.Combine(OsuDirectory, "Songs");
 	}
 }
