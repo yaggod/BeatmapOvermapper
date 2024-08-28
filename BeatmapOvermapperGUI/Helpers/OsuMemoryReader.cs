9ad19f3d7325b15c438619b8ce98a8b52bb8484d
yaggod <yaggod@yandex.ru>
2024-08-28 05:26:04 +0300

    create static beatmap reader

---
+++ BeatmapOvermapperGUI/Helpers/OsuMemoryReader.cs
@@ -0,0 +1,28 @@
+﻿using BeatmapOvermapperGUI.Models;
+using OsuMemoryDataProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatmapOvermapperGUI.Helpers
+{
+    public static class OsuMemoryReader
+    {
+        private static StructuredOsuMemoryReader _memoryReader = StructuredOsuMemoryReader.Instance;
+
+        public static BeatmapWrapper GetCurrentBeatmap()
+        {
+            _memoryReader.TryRead(_memoryReader.OsuMemoryAddresses.Beatmap);
+            string folderName = _memoryReader.OsuMemoryAddresses.Beatmap.FolderName;
+            string osuFileName = _memoryReader.OsuMemoryAddresses.Beatmap.OsuFileName;
+
+            return new BeatmapWrapper()
+            {
+                FolderName = folderName,
+                OsuFileName = osuFileName
+            };
+        }
+    }
+}
---
+++ BeatmapOvermapperGUI/Models/BeatmapWrapper.cs
@@ -0,0 +1,32 @@
+﻿using Coosu.Beatmap;
+using ProcessMemoryDataFinder.API;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatmapOvermapperGUI.Models
+{
+	public struct BeatmapWrapper
+	{
+		private OsuFile _osuFile;
+
+		public string FolderName { get; set; }
+		public string OsuFileName { get; set; }
+		public string BeatmapFolderPath => Path.Combine(Settings.SongsFolder, FolderName);
+		public string FullPath => Path.Combine(BeatmapFolderPath, OsuFileName);
+
+		public OsuFile OsuFile
+		{
+			get
+			{
+				if (_osuFile == null)
+					_osuFile = OsuFile.ReadFromFile(FullPath);
+				return _osuFile;
+			}
+		}
+
+	}
+}
